f9ec317d977828f6971e9d2a3e55927203211bd6
Rasel Chowdhury <89883537+Raselll@users.noreply.github.com>
2023-05-28 09:12:30 -0400

    Add files via upload

---
+++ c01-l02-equipe6-main/Exercises/Developpeur.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <string>
+#include "gsl/span"
+#include "ListeJeux.hpp"
+
+class Developpeur
+{
+private:
+
+	std::pair<std::string, ListeJeux> paireNomJeux_;
+
+public:
+
+	Developpeur(const std::string& name = "   ");
+	~Developpeur();
+
+	void addGames(ListeJeux& gameList);
+	void display() const;
+	const std::string& getName() const { return paireNomJeux_.first; }
+	int countNbGameDevelopped(ListeJeux& gameList);
+};
---
+++ c01-l04-equipe6-main/Exercises/tp4/Heros.h
@@ -0,0 +1,55 @@
+﻿/**
+* Header pour la classe Heros qui herite de la classe Personnage.
+* \file   Heros.h
+* \author Bakir, Farid (1908977), Chowdhury, Rasel (2143023) et Tao, Tristan (1951367)
+* \date   05 juin 2022
+* Créé le 01 juin 2022
+*/
+
+#pragma once
+#include <vector>
+#include <memory>
+#include <iostream>
+
+#include "Personnage.h"
+
+using namespace std;
+
+class Heros : virtual public Personnage
+{
+public:
+	Heros(const string& nom, const string& jeu, unsigned nAllies, string& ennemi) : Personnage(nom, jeu), nAllie_(nAllies), ennemi_(ennemi) {}
+	Heros(const Heros& autre);
+	void afficher(ostream& os) override;
+	void ajouterAllies(shared_ptr<string> nom) { listeAllie_.push_back(nom); }
+
+	string getEnnemi() const { return ennemi_; }
+	vector<shared_ptr<string>> getAllies() const { return listeAllie_; }
+
+private:
+	unsigned nAllie_;
+	string ennemi_;
+	vector<shared_ptr<string>> listeAllie_;
+};
+
+void Heros::afficher(ostream& os)
+{
+	Personnage::afficher(os);
+	Personnage::changerCouleur(os, 'R');
+	os << "Ennemi : " << ennemi_ << "\n\n";
+	os << "Allie : " << "\n";
+	for (auto&& allies : listeAllie_) 
+	{
+		os << "  " << *allies << "\n";
+	}
+	os << "═════════════════════════════════════════════════════════════════════════" << "\n";
+
+}
+
+Heros::Heros(const Heros& autre) : Personnage(autre)
+{
+	ennemi_ = autre.ennemi_;
+	nAllie_ = autre.nAllie_;
+	for (auto&& allie : autre.listeAllie_)
+		listeAllie_.push_back(allie);
+}
